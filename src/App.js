c6585db3fede4082f5a375a7e25b43777be9049d
devgreact <dev.greact@gmail.com>
2023-11-30 15:45:53 +0900

    docs :  연습해보기 css 후 emotion 대기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./styles/App.css";
 
 function App() {
-  return <div>안녕</div>;
+  return (
+    <div className="layout">
+      <div className="wrap">
+        <header></header>
+        <main></main>
+        <footer></footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
